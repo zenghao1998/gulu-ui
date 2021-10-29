0b19e7756080defea674492104426cec10464093
a1248437410 <wcf5201314@>
2021-10-29 21:44:03 +0800

    添加了input组件

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Button from './button'
-import buttonGroup from './button-group'
-Vue.component('g-button',Button)
-Vue.component('g-button-group',buttonGroup)
+import ButtonGroup from './button-group'
+import Input from './input'
+Vue.component('g-button', Button)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 new Vue({
-    data:{
-        msg:12
+    data: {
+        msg: 12
     },
 }).$mount("#app")
